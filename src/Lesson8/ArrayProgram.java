fb566c43e38b2bfbb43c3cfdfe81c04c75d07a23
sem-sydorov <sydorovbm@gmail.com>
2023-02-05 18:19:04 +0200

    Homework 8 done!

---
+++ src/Lesson8/ArrayProgram.java
@@ -0,0 +1,74 @@
+package Lesson8;
+
+public class ArrayProgram {
+
+    public static void main(String[] args) {
+
+        //Create our array
+        int[] array = new int[]{4, 6, 8, 23, 17, 98, 45, 32, 77, 100};
+
+
+        //Output our array in console
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        //Find the minimum number of an array
+        System.out.println(getMinValue(array));
+
+        //Find the max number of an array
+        System.out.println(getMaxValue(array));
+
+        //Find the average of an array
+        System.out.println(getAverageArray(array));
+
+        //Find the sum of an array
+        System.out.println(getSumArray(array));
+
+
+    }
+
+    //Method getting the minimum value
+    static int getMinValue(int[] array) {
+        int minValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    }
+
+    //Method getting the max value
+    static int getMaxValue(int[] array) {
+        int maxValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+
+    // Method getting average of an array
+    static int getAverageArray(int[] array) {
+        int sum = 0;
+        int average;
+
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        average = (int) sum / array.length;
+        return average;
+
+    }
+
+    // Method getting sum of an array
+    static int getSumArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; sum += array[i++]);
+        return sum;
+    }
+
+}
